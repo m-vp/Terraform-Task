877ddbe9eb6291155fdd3b838f08d9621b2c3024
m-vp <bl.en.u4cse22236@bl.students.amrita.edu>
2026-04-07 18:41:35 +0530

    made changes

--- task03/main.tf
+++ task03/main.tf
@@ -5,39 +5,39 @@ resource "azurerm_resource_group" "rg" {
   tags = var.tags
 }
 
-
-
-resource "azurerm_storage_account" "SA" {
+resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
   tags = var.tags
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
+  address_space = var.vnet_address_space
 
   tags = var.tags
 }
 
 resource "azurerm_subnet" "frontend" {
-    name = var.frontend_subnet_name
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.example.name
-    address_prefixes = ["10.0.1.0/24"]
+  name                 = var.frontend_subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = var.frontend_subnet_prefix
 }
 
-resource "azurerm_subnet" "example" {
-    name = var.backend_subnet_name
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.example.name
-    address_prefixes = ["10.0.2.0/24"]
+resource "azurerm_subnet" "backend" {
+  name                 = var.backend_subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = var.backend_subnet_prefix
 }
