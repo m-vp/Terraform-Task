7ef1c8fa7e84458aa93333e1b5bb80f7d899e18c
m-vp <bl.en.u4cse22236@bl.students.amrita.edu>
2026-04-13 11:20:42 +0530

    task done

--- task04/main.tf
+++ task04/main.tf
@@ -1,6 +1,8 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
-  location = "North Europe"
+  location = "East US"
+
+  tags = var.Tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -9,26 +11,36 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 
   address_space = ["10.0.0.0/16"]
+
+  tags = var.Tags
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+
+  address_prefixes = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
   name                = var.public_ip_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+
+  allocation_method = "Static"
+
+  domain_name_label = var.dns_label_name
+
+  tags = var.Tags
 }
 
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = var.Tags
 }
 
 resource "azurerm_network_security_rule" "http" {
@@ -72,9 +84,12 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
     public_ip_address_id          = azurerm_public_ip.pip.id
     private_ip_address_allocation = "Dynamic"
   }
+
+  tags = var.Tags
 }
 
 resource "azurerm_network_interface_security_group_association" "nic_nsg" {
@@ -83,6 +98,50 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name = var.vm_name
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
+  size = var.vm_sku
+
+  admin_username = "azureuser"
+  admin_password = var.vm_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  tags = var.Tags
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y nginx",
+      "sudo systemctl enable nginx",
+      "sudo systemctl start nginx"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "azureuser"
+      password = var.vm_password
+      host     = azurerm_public_ip.pip.ip_address
+    }
+  }
+
+  depends_on = [azurerm_network_interface.nic]
 }
