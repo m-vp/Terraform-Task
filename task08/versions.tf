bc0b025044874f6f570d4424b7448c31837aa78b
m-vp <bl.en.u4cse22236@bl.students.amrita.edu>
2026-04-28 10:11:06 +0530

    fixed issue regarding providers

--- task08/versions.tf
+++ task08/versions.tf
@@ -30,17 +30,7 @@ provider "azurerm" {
 }
 
 
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
+
 
 provider "kubernetes" {
   host                   = module.aks.host
