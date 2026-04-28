13aa4c7244314f4312c57b7fd72052eb571d6ba8
m-vp <bl.en.u4cse22236@bl.students.amrita.edu>
2026-04-28 10:06:47 +0530

    fixed issue in locals

--- task08/locals.tf
+++ task08/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  rg_name    = join("-", [var.name_prefix, "rg"])
-  redis_name = join("-", [var.name_prefix, "redis"])
-  keyvault_name    = join("-", [var.name_prefix, "kv"])
-  aci_name   = join("-", [var.name_prefix, "ci"])
-  aks_name   = join("-", [var.name_prefix, "aks"])
-  acr_name   = "cmtrw7gf0vkrmod8cr"
+  rg_name       = join("-", [var.name_prefix, "rg"])
+  redis_name    = join("-", [var.name_prefix, "redis"])
+  keyvault_name = join("-", [var.name_prefix, "kv"])
+  aci_name      = join("-", [var.name_prefix, "ci"])
+  aks_name      = join("-", [var.name_prefix, "aks"])
+  acr_name      = "cmtrw7gf0vkrmod8cr"
 
 }
